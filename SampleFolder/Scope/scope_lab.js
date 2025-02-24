45571a08912b1315f4c38fa35fe62bbc9c85b875
DerPommedeTerre <administrador@MacBook-Pro-de-Administrador.local>
2025-02-24 14:41:37 -0600

    Practice Task

--- SampleFolder/Scope/scope_lab.js
+++ SampleFolder/Scope/scope_lab.js
@@ -31,3 +31,18 @@ show();
 console.log(functionVar); // Throws ReferenceError
 console.log(functionLet); // Throws ReferenceError
 console.log(functionConst); // Throws ReferenceError
+
+//Practice Task - Variable reassignment
+function newBlock(){
+    var varOne = "var one";
+    let letOne = "let one";
+    const constoOe = "const one";
+
+    var varOne = "var two";
+    let letOne = "let two";
+    const constOne = "const two";
+}
+
+var varOne = "var three";
+let letOne = "let three";
+const constOne = "const three";
